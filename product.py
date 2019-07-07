d75717244f491d4f6c087154bbe6bf02d1f22d05
LeO So <leoso3037@gmail.com>
2019-07-07 17:20:39 +0800

    new program

---
+++ product.py
@@ -0,0 +1,11 @@
+products = []
+while True:
+	name = input('Please enter the product name:')
+	if name == 'q':
+		break
+	price = input('Please enter the product price')
+	products.append([name, price])
+print(products)
+
+for p in products:
+	print('The price of', p[0], 'is' ,p[1], '.' )
