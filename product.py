7fd439f57e86de05fa5b682b9ce0593b30babfad
LeO So <leoso3037@gmail.com>
2019-07-11 16:09:35 +0800

    Check if file is exceed

--- product.py
+++ product.py
@@ -1,12 +1,19 @@
+import os
 products = []
-with open('products.csv', 'r', encoding = 'utf-8 ') as f:
-	for line in f:
-		if 'Product,Price' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+# Read files
+if os.path.isfile('product.csv')
+	print('File is here!!!!')
+	with open('products.csv', 'r', encoding = 'utf-8 ') as f:
+		for line in f:
+			if 'Product,Price' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('Where is the file???')
 
+#Input
 while True:
 	name = input('Please enter the product name:')
 	if name == 'q':
@@ -15,9 +22,11 @@ while True:
 	products.append([name, price])
 print(products)
 
+#Print all the records
 for p in products:
 	print('The price of', p[0], 'is' ,p[1], '.' )
 
+#Write to files
 with open('products.csv', 'w', encoding = 'utf-8 ') as f:
 		for p in products:
 			f.write(p[0] + ',' + p[1] + '\n')
